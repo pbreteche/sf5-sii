ffb62abce10d8f332d33db9969654c42eb3e52d8
Pierre Bretéché <pbreteche@dawan.fr>
2019-12-18 12:26:21 +0100

    Paramètre de route

--- src/Controller/RestApiController.php
+++ src/Controller/RestApiController.php
@@ -5,15 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/api")
+ */
 class RestApiController extends AbstractController
 {
     /**
-     * @Route("/api")
+     * @Route("")
      */
     public function index()
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
+        ]);
+    }
+
+    /**
+     * @Route("/{name}", methods="GET")
+     */
+    public function hello(string $name)
+    {
+        return $this->json([
+            'message' => 'Salut '.$name.'!',
         ]);
     }
 }
