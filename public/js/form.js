8d4092bf5f5d1e891848826642e2bcb307321e0e
Pierre Bretéché <pbreteche@dawan.fr>
2019-12-19 16:11:55 +0100

    recherche en AJAX

---
+++ public/js/form.js
@@ -0,0 +1,15 @@
+const searchFormElement = document.querySelector('#search-form');
+const searchInputElement = searchFormElement.querySelector('input');
+
+searchFormElement.addEventListener('submit', function(event){
+    event.preventDefault();
+    fetch('/search?search-term='+searchInputElement.value)
+            .then(response => response.json())
+            .then(data => {
+                const listElements = data.reduce((accumulator, article) =>
+                        accumulator + `<li><a href="">${article.title}</a></li>`, '');
+                const resultElement = document.createElement('ul');
+                resultElement.innerHTML = listElements;
+                searchInputElement.after(resultElement)
+            })
+});
